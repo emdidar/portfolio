ca4927a85a0d6c4945fa3ee34e6f0b5c9d46806e
Mohammad Didarul Alam <emdidar@gmail.com>
2022-05-11 02:05:05 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,45 @@
-/************************
-    Smooth Scrolling
-************************/
 $(document).ready(function () {
-    //add smooth scrolling to all links
-    $("a").on('click', function (event) {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-        //if link is an anchor link
-        if (this.hash !== "") {
+      // Store hash
+      var hash = this.hash;
 
-            //prevent default behavior
-            event.preventDefault();
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
 
-            //get the hash value
-            var hash = this.hash;
+  $(".navbar-toggle").on("click", function () {
+    $(".mobile-navbar").toggleClass("mobile-navbar-sidebar-open");
+    $("#toggle-icon").toggleClass("fa-times");
+  });
 
-            //use jQuery's animate() method to add smooth page scroll
-            //the optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
+  $(window).click(function () {
+    $(".mobile-navbar").removeClass("mobile-navbar-sidebar-open");
+    $("#toggle-icon").addClass("fa-bars");
+    $("#toggle-icon").removeClass("fa-times");
+  });
 
-                //add hash (#) to URL when done scrolling
-                window.location.hash = hash;
+  $(".navbar-toggle").on("click", function (event) {
+    event.stopPropagation();
+  });
 
-            });
-        } //end if
-    });
+  $(".mobile-navbar").on("click", function (event) {
+    event.stopPropagation();
+  });
 });
